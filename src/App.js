2dd640a0c20594af53a50b9f8a6e88b3e9d0e0a7
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-28 13:03:49 -0400

    more stylinf for display container

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header setIPAddress={setIPAddress} />
-      <Display />
+      <Display IPAddress={IPAddress} />
       <Map />
     </div>
   );
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Data from "./Data";
 
-export default function Display() {
+export default function Display({IPAddress}) {
     return (
         <section className="display-container">
-            
+            <Data name="IP Address" data={IPAddress} />
+            <Data name="LOCATION" data={null} />
+            <Data name="TIMEZONE" data={null} />
+            <Data name="ISP" data={null} />
         </section>
     )
 }
