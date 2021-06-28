e187580f114e60cb3b401ce632e8f4bf1ae465bc
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-28 10:10:23 -0400

    added components and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Display from "./components/Display";
+import Header from "./components/Header";
+import Map from "./components/Map";
 
-import './App.css';
 
 function App() {
-  const IPurl = "https://geo-database.ipify.org/datafeeds";
+  const [IPAddress, setIPAddress] = useState("")
+  const IPurl = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_API_KEY}&ipAddress=8.8.8.8`;
   return (
     <div className="App">
+      <Header setIPAddress={setIPAddress} />
+      <Display />
+      <Map />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import background from "../images/pattern-bg.png"
+
+export default function Header({setIPAddress}) {
+    return (
+        <header style={{backgroundImage: background}}>
+            <h1>IP Address Tracker</h1>
+            <input 
+            type="text" 
+            placeholder="Search for any IP adress or domain"
+            onChange={e => setIPAddress(e.target.value)}
+            />
+        </header>
+    )
+}
