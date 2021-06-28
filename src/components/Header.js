ed20b4748d2cf638909a3b567a3c1176e9af1ee8
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-28 12:48:04 -0400

    almost finsihed styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import background from "../images/pattern-bg.png"
 
 export default function Header({setIPAddress}) {
     return (
         <header>
             <h1>IP Address Tracker</h1>
-            <input 
-            type="text" 
-            placeholder="Search for any IP adress or domain"
-            onChange={e => setIPAddress(e.target.value)}
-            />
+            <span>
+                <input 
+                type="text" 
+                placeholder="Search for any IP adress or domain"
+                onChange={e => setIPAddress(e.target.value)}
+                >
+                </input>
+                <button></button>
+            </span>
         </header>
     )
 }
